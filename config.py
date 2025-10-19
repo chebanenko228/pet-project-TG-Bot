baae8008acf7f033f7c587b8c1a542a883e105b8
Vladimir <volodymyr.chebanenko.kita@donntu.edu.ua>
2025-10-19 23:13:02 +0300

    docs: include .env.example template

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 from aiogram.client.bot import DefaultBotProperties
 from aiogram import Bot, Dispatcher
 from datetime import datetime, timezone
+from environs import Env
 
 BOT_START_TIME = datetime.now(timezone.utc)
 
-API_TOKEN = ""
-ADMIN_IDS = []  # ID администраторов
-GROUP_ID = -100  # ID канала
-DB_PATH = ".db" # Название файла базы данных
+env = Env()
+env.read_env()  # подгрузит .env
+
+API_TOKEN = env.str("API_TOKEN", "")
+ADMIN_IDS = env.list("ADMIN_IDS", subcast=int)
+GROUP_ID = env.int("GROUP_ID", 0)
+DB_PATH = env.str("DB_PATH", "group_access.db")
 
 bot = Bot(
     token=API_TOKEN,
